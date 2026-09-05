3b9091b5e917be517cd004566f0c70d8f3328283
HooriaA <hooriaaziz28@gmail.com>
2026-09-05 05:24:42 -0500

    Update Sketch.ino

--- Sketch.ino
+++ Sketch.ino
@@ -1,8 +1,13 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include "OledAn.h"
+#include "ConfusedFra.h"
+#include "AngryFra.h"
+#include "HappyFra.h"
+
+enum moods {confused, angry, happy}; //only for simulation I will be adding 3 emotion states. The actual version will have more than thsi
 
 float getDistance();
+void emotions();
 
 //OLED
 #define SCREEN_I2C_ADDR 0x3C // or 0x3C
@@ -15,7 +20,10 @@ Adafruit_SSD1306 display(128, 64, &Wire, OLED_RST_PIN);
 //sensor pinout
 int ECHO = 18; 
 int TRIG = 5;
+
 int frame =0;
+
+
 
 //SCL 22
 //SDA 21
@@ -33,29 +41,34 @@ void setup() {
 
 void loop() {
   float distance = getDistance();
-     
-  display.clearDisplay();
-  display.drawBitmap(32, 0, frames[frame], FRAME_WIDTH, FRAME_HEIGHT, 1);
-  display.display();
-  frame = (frame + 1) % FRAME_COUNT;
-  delay(FRAME_DELAY);
 
-  if (distance < 20.0)
+  if(distance < 5)
+  {
+    Serial.println("Stopped");
+    emotions(angry);
+  }
+
+  else if (distance < 20.0)
   {
     Serial.println("Object in front");
+    emotions(confused);
 
     int turn = random(0, 2);
-    if( turn == 0)
-    {
+    if( turn == 0) {
       Serial.println("Turn right");
       //turn left =false;
     }
-    else
-    {
+
+    else{
       Serial.println("Turn left");
       //turn right = false;
     }
   }
+
+   else
+   {
+    emotions(happy);
+   }
 }
 
 //function to get the distance
@@ -76,3 +89,36 @@ float getDistance() {
 
   return distance;
 }
+
+void emotions(moods mood)
+{
+  display.clearDisplay();
+
+  switch (mood) {
+    case confused:
+      display.drawBitmap(32, 0, Confusedframes[frame], ConfuFRAME_WIDTH, ConfuFRAME_HEIGHT, 1);
+      frame = (frame + 1) % ConfuFRAME_COUNT;
+      
+      break;
+      case angry:
+      display.drawBitmap(40, 8, Angryframes[frame], AngFRAME_WIDTH, AngFRAME_HEIGHT, 
+      1);
+      frame = (frame + 1) % AngFRAME_COUNT;
+
+      break; 
+      case happy:
+        display.drawBitmap(40, 8, Happyframes[frame], HapFRAME_WIDTH, HapFRAME_HEIGHT, 1);
+        frame = (frame + 1) % HapFRAME_COUNT;
+
+      break;
+
+      default:
+      Serial.println("HOW DID YOU GET HERE?");
+      break;      
+  
+  }
+   display.display();
+   delay(FRAME_DELAY);
+    
+  }
+  

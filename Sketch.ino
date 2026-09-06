358b5c010dd4ba2ce02c438f05fbcc60775700f5
HooriaA <hooriaaziz28@gmail.com>
2026-09-05 20:00:53 -0500

    Update Sketch.ino
    Added the right code

--- Sketch.ino
+++ Sketch.ino
@@ -1,84 +1,146 @@
-int leftMortPos = 14; //in2
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "ConfusedFra.h"
+#include "AngryFra.h"
+#include "HappyFra.h"
+#include "motion.h"
+
+enum moods {confused, angry, happy}; //only for simulation I will be adding 3 emotion states. The actual version will have more than thsi
+
+float getDistance();
+void emotions(moods mood);
+
+//OLED
+#define SCREEN_I2C_ADDR 0x3C // or 0x3C
+#define SCREEN_WIDTH 128     // OLED display width, in pixels
+#define SCREEN_HEIGHT 64     // OLED display height, in pixels
+#define OLED_RST_PIN -1      // Reset pin (-1 if not available)
+
+Adafruit_SSD1306 display(128, 64, &Wire, OLED_RST_PIN);
+
+//sensor pinout
+int ECHO = 18; 
+int TRIG = 5;
+
+
+//Motor pinout
+/*int leftMortPos = 14; //in2
 int leftMortNeg = 4; //in1
 int rihgtMorPos = 27; //in3
 int rihgtMorNeg =23; //in4
 int ENA2= 12;
 int ENA1 = 26;
-
-int fast =255;
-int slow = 100;
-
-void moveForward()
-{
-    digitalWrite(ENA2, HIGH);
-    digitalWrite(ENA1, HIGH);
+*/
 
 
-    digitalWrite(leftMortPos, HIGH);
-    digitalWrite(leftMortNeg, LOW);
+int frame =0;
 
-    digitalWrite(rihgtMorPos, HIGH);
-    digitalWrite(rihgtMorNeg, LOW);
 
-    Serial.println("Continue walking");
+void setup() {
+  pinMode(ECHO, INPUT);
+  pinMode(TRIG, OUTPUT);
+  pinMode(leftMortNeg, OUTPUT);
+  pinMode(leftMortPos, OUTPUT);
+  pinMode(rihgtMorNeg, OUTPUT);
+  pinMode(rihgtMorPos, OUTPUT);
+  pinMode(ENA1, OUTPUT);
+  pinMode(ENA2, OUTPUT);
+
+  Serial.begin(115200);
+
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_I2C_ADDR);
+   
 }
 
-void reverse()
-{
-    digitalWrite(ENA1, HIGH);
-    digitalWrite(ENA2, HIGH);
+void loop() {
 
-    digitalWrite(leftMortPos, LOW);
-    digitalWrite(leftMortNeg, HIGH);
-
-    digitalWrite(rihgtMorPos, LOW);
-    digitalWrite(rihgtMorNeg, HIGH);
-
-    Serial.println("Reverse now");
-}
-
-void STop()
-{
-    digitalWrite(ENA1, LOW);
-    digitalWrite(ENA2, LOW);
-
-    digitalWrite(leftMortPos, LOW);
-    digitalWrite(leftMortNeg, LOW);
-
-    digitalWrite(rihgtMorPos, LOW);
-    digitalWrite(rihgtMorNeg, LOW);
-
-    Serial.println("Stopping");
-}
-
-void rightTurn()
-{
-
-    digitalWrite(ENA1, HIGH);
-    digitalWrite(ENA2, LOW);
-
-    digitalWrite(leftMortPos, HIGH);
-    digitalWrite(leftMortNeg, LOW);
-
-    digitalWrite(rihgtMorPos, LOW);
-    digitalWrite(rihgtMorNeg, LOW);
-
-    Serial.println("Turning right");
-}
-
-void leftTurn()
-{
-
-    digitalWrite(ENA1, LOW);
-    digitalWrite(ENA2, HIGH);
-
-    digitalWrite(leftMortPos, LOW);
-    digitalWrite(leftMortNeg, LOW);
-
-    digitalWrite(rihgtMorPos, HIGH);
-    digitalWrite(rihgtMorNeg, LOW);
-
-    Serial.println("Turning left");
-
-}
   
+  float distance = getDistance();
+
+  if(distance < 5)
+  {
+    Serial.println("Stopped");
+    emotions(angry);
+    STop();
+  }
+
+  else if (distance < 20.0)
+  {
+    Serial.println("Object in front");
+    emotions(confused);
+
+    int turn = random(0, 2);
+    if( turn == 0) {
+      Serial.println("Turn right");
+      rightTurn();
+      delay(3000);
+      STop();
+   
+    }
+
+    else{
+      Serial.println("Turn left");
+      //turn right = false;
+      leftTurn();
+      delay(3000);
+      STop();
+    }
+  }
+
+   else
+   {
+    emotions(happy);
+    moveForward();
+   }
+}
+
+//function to get the distance
+float getDistance() {
+  digitalWrite(TRIG, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIG, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(TRIG, LOW);
+
+  int duration = pulseIn(ECHO, HIGH, 30000);
+  float distance = duration * 0.034 / 2;
+
+  Serial.print("Distance ");
+  Serial.println(distance);
+
+  return distance;
+}
+
+void emotions(moods mood)
+{
+  display.clearDisplay();
+
+  switch (mood) {
+    case confused:
+      display.drawBitmap(32, 0, Confusedframes[frame], ConfuFRAME_WIDTH, ConfuFRAME_HEIGHT, 1);
+      frame = (frame + 1) % ConfuFRAME_COUNT;
+      
+      break;
+      case angry:
+      display.drawBitmap(40, 8, Angryframes[frame], AngFRAME_WIDTH, AngFRAME_HEIGHT, 
+      1);
+      frame = (frame + 1) % AngFRAME_COUNT;
+
+      break; 
+      case happy:
+        display.drawBitmap(40, 8, Happyframes[frame], HapFRAME_WIDTH, HapFRAME_HEIGHT, 1);
+        frame = (frame + 1) % HapFRAME_COUNT;
+
+      break;
+
+      default:
+      Serial.println("HOW DID YOU GET HERE?");
+      break;      
+  
+  }
+   display.display();
+   delay(FRAME_DELAY);
+    
+  }
